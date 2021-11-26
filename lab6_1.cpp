97deb50cb593813c959753d8aadfbe0d1e26cef3
bewzunjo <saran_kim@cmu.ac.th>
2021-11-26 16:34:17 +0700

    ku ke

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
-
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main(){2
+int i = -1;
+int y;
+int ku = 0;
+int ke = 0;
+while (i < 0)
+{
+        cout << "Enter an integer: ";
+        cin >> y;
+        if(y == 0){
+        i=1;
+    }else{
+            
+            if(y%2 == 0){
+            ku=ku+1;
+        }
+            else{
+            ke=ke+1;
+        }
+    }
+}
+    cout << "#Even numbers = " << ku << "\n";
+    cout << "#Odd numbers = " << ke;
     return 0;
 }
